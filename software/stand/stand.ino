d1858496ab622722c88cdcde4ab43bf6e3ad21fa
Luis Diaz <luis.diaz@bq.com>
2015-10-29 09:29:26 +0100

    Added some movements

--- software/stand/stand.ino
+++ software/stand/stand.ino
@@ -21,11 +21,12 @@ bool calibrated = false;
 void setup() {
   Serial.begin(19200);
 
-  Serial.println("Init");
-  Serial.println("-----------------------");
   initServos();
+  midServosStable();
+  delay(50);
   standDown();
-  delay(500);
+  delay(50);
+  Serial.println("Ready!");
 }
 int servo;
 int pos;
@@ -34,32 +35,50 @@ void loop() {
 
   if (Serial.available() > 0) {
     char incomming = Serial.read();
-    Serial.print("incomming = ");
-    Serial.println(incomming);
 
     if(incomming == 'A'){
       Serial.println("A");
+      midServosStable();
       standDown();
     }
     if(incomming == 'B'){
       Serial.println("B");
+      midServosStable();
       standUp();
-    }else{
+    }
+    if(incomming == 'C'){
+      Serial.println("C");
+      midServosStable();
+      makeX();
+    }
+    if(incomming == 'D'){
+      Serial.println("D");
+      midServosStable();
+    }
+    if(incomming == 'E'){
+      Serial.println("E");
+      midServosStable();
+      cross();
+    }
+    if(incomming == 'F'){
+      Serial.println("F");
+      midServosStable();
+      lift();
+    }
+    if (incomming == 'S'){
+      Serial.println("S");
       servo = Serial.parseInt();
       pos = Serial.parseInt();
+      if(servo != 0 && servo <= servos){
+        Serial.print(servo);
+        Serial.print(",");
+        Serial.println(pos);
+        manageServos(servo, pos);
+      }
+      servo = 0;
+      pos = 90;
     }
 
-    if(servo != 0){
-      Serial.print(servo);
-      Serial.print(",");
-      Serial.println(pos);
-      manageServos(servo, pos);
-    }
-
-
-    if (Serial.read() == '\n') {
-      Serial.println("dentro de \\n");
-    }
   }
 
 }
@@ -110,7 +129,7 @@ void manageServos(int _servo, int _pos) {
   delay(50);
   Serial.print("Done with servo");
   Serial.print(_servo);
-  Serial.println("---------");
+  Serial.println("\n---------");
 }
 void initServos(){
     servo1.attach(2);
@@ -125,41 +144,50 @@ void initServos(){
     servo10.attach(11);
     servo11.attach(12);
     servo12.attach(13);
+    cross();
+    delay(10);
+    standDown();
+}
+void makeX(){
+  servo3.write(90);
+  servo4.write(90);
+  servo7.write(90);
+  servo10.write(90);
+}
+void midServosStable(){
+  servo1.write(50);
+  servo5.write(85);
+  servo8.write(90);
+  servo11.write(80);
 }
 void standUp(){
-  servo1.write(40);
+  //move inner servos
+  makeX();
+  delay(50);
+  //move central servos
+  midServosStable();
+  delay(50);
+  //lift
+  lift();
   delay(10);
-  servo2.write(90);
-  delay(10);
-  servo3.write(90);
-  delay(10);
-  servo4.write(90);
-  delay(10);
-  servo5.write(90);
-  delay(10);
-  servo6.write(90);
-  delay(10);
-  servo7.write(90);
-  delay(10);
-  servo8.write(90);
-  delay(10);
-  servo9.write(70);
-  delay(10);
-  servo10.write(90);
-  delay(10);
-  servo11.write(90);
-  delay(10);
-  servo12.write(110);
 }
 void standDown(){
   servo1.write(50);
-  delay(10);
+  servo5.write(85);
   servo2.write(170);
-  delay(10);
   servo6.write(30);
-  delay(10);
-  servo9.write(140);
-  delay(10);
-  servo12.write(20);
-
+  servo9.write(135);
+  servo12.write(15);
+}
+void cross(){
+  servo3.write(130);
+  servo4.write(65);
+  servo7.write(130);
+  servo10.write(45);
+}
+void lift(){
+  servo2.write(90);
+  servo6.write(90);
+  servo9.write(70);
+  servo12.write(80);
 }
