2ab8fdf5b259e49fa98b62d281285673c8681fae
LuisDiazUgena <luis.diaz@bq.com>
2015-11-02 16:10:08 +0100

    Now library works without any trouble

--- software/SpiderRobot/SpiderRobot.ino
+++ software/SpiderRobot/SpiderRobot.ino
@@ -1,7 +1,14 @@
 #include "Spider.h"
+#include "Servo.h"
 
 Spider spider(2,3,4,5,6,7,8,9,10,11,12,13);
 
 void setup() {
     spider.init();
+}
+
+void loop(){
+    spider.standUp();
+    delay(1000);
+    spider.standDown();
 }
