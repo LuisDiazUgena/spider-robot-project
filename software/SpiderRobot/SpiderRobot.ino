18ee3a3673357b556d43f878006a154cbfe4c6d9
LuisDiazUgena <luis.diaz@bq.com>
2015-11-02 18:39:29 +0100

    Added some code for testing proposes

--- software/SpiderRobot/SpiderRobot.ino
+++ software/SpiderRobot/SpiderRobot.ino
@@ -1,14 +1,17 @@
 #include "Spider.h"
 #include "Servo.h"
 
-Spider spider(2,3,4,5,6,7,8,9,10,11,12,13);
+Spider spider(2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13);
 
 void setup() {
-    spider.init();
+  Serial.begin(19200);
+  Serial.println("Hola");
+  delay(100);
+  spider.initialize();
 }
 
-void loop(){
-    spider.standUp();
-    delay(1000);
-    spider.standDown();
+void loop() {
+  spider.standUp();
+  delay(1000);
+  spider.standDown();
 }
